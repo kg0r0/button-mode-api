04a8e5bd2a0cdd812fa289b823805c720bee9fab
kg0r0 <kg0r0@yahoo.co.jp>
2024-04-23 23:41:43 +0900

    Add some comments to each endpoint

--- idp/main.go
+++ idp/main.go
@@ -43,6 +43,52 @@ func main() {
 	http.ListenAndServe(":"+port, nil)
 }
 
+// accountsHandler implements the accounts endpoint.
+// Ref: https://developers.google.com/privacy-sandbox/3pcd/fedcm-developer-guide#accounts-list-endpoint
+func accountsHandler(w http.ResponseWriter, r *http.Request) {
+	jsonResponse(w, map[string]interface{}{"accounts": []map[string]string{{"id": "1234", "name": "John Doe", "email": "john_doe@idp.example"}}}, http.StatusOK)
+}
+
+// webIdentityHandler implements the well-known/web-identity endpoint.
+// Ref: https://developers.google.com/privacy-sandbox/3pcd/fedcm-developer-guide#well-known-file
+func webIdentityHandler(w http.ResponseWriter, r *http.Request) {
+	jsonResponse(w, map[string]string{"provider_urls": "http://localhost:8002/config.json"}, http.StatusOK)
+}
+
+// fedcmAssertionHandler implements the ID assertion endpoint.
+// Ref: https://developers.google.com/privacy-sandbox/3pcd/fedcm-developer-guide#id-assertion-endpoint
+func fedcmAssertionHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8001")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	jsonResponse(w, map[string]string{"token": "*****"}, http.StatusOK)
+}
+
+// configJSONHandler implements the IdP config file endpoint.
+// Ref: https://developers.google.com/privacy-sandbox/3pcd/fedcm-developer-guide#idp-config-file
+func configJSONHandler(w http.ResponseWriter, r *http.Request) {
+	jsonResponse(w, map[string]interface{}{
+		"accounts_endpoint":        "/accounts",
+		"client_metadata_endpoint": "/metadata",
+		"id_assertion_endpoint":    "/fedcm_assertion_endpoint",
+		"disconnect_endpoint":      "/disconnect",
+		"login_url":                "/login",
+	}, http.StatusOK)
+}
+
+// clientMetadataHandler implements the client metadata endpoint.
+// Ref: https://developers.google.com/privacy-sandbox/3pcd/fedcm-developer-guide#client-metadata-endpoint
+func clientMetadataHandler(w http.ResponseWriter, r *http.Request) {
+	clientID := r.FormValue("client_id")
+	if clientID != "123" {
+		jsonResponse(w, map[string]string{"error": "invalid client_id."}, http.StatusBadRequest)
+		return
+	}
+	jsonResponse(w, map[string]string{"privacy_policy_url": "http://localhost:8001/privacy_policy.html", "terms_of_service_url": "http://localhost:8001/terms_of_service.html"}, http.StatusOK)
+}
+
+// loginHandler implements the login_url endpoint.
+// Ref: https://developers.google.com/privacy-sandbox/blog/fedcm-chrome-120-updates
 func loginHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "login.html", nil)
 }
@@ -74,40 +120,6 @@ func signInHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Set-Login", "logged-in")
 	jsonResponse(w, map[string]string{"message": "success"}, http.StatusOK)
-}
-
-func accountsHandler(w http.ResponseWriter, r *http.Request) {
-	jsonResponse(w, map[string]interface{}{"accounts": []map[string]string{{"id": "1234", "name": "John Doe", "email": "john_doe@idp.example"}}}, http.StatusOK)
-}
-
-func webIdentityHandler(w http.ResponseWriter, r *http.Request) {
-	jsonResponse(w, map[string]string{"provider_urls": "http://localhost:8002/config.json"}, http.StatusOK)
-}
-
-func fedcmAssertionHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8001")
-	w.Header().Set("Access-Control-Allow-Credentials", "true")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-	jsonResponse(w, map[string]string{"token": "*****"}, http.StatusOK)
-}
-
-func configJSONHandler(w http.ResponseWriter, r *http.Request) {
-	jsonResponse(w, map[string]interface{}{
-		"accounts_endpoint":        "/accounts",
-		"client_metadata_endpoint": "/metadata",
-		"id_assertion_endpoint":    "/fedcm_assertion_endpoint",
-		"disconnect_endpoint":      "/disconnect",
-		"login_url":                "/login",
-	}, http.StatusOK)
-}
-
-func clientMetadataHandler(w http.ResponseWriter, r *http.Request) {
-	clientID := r.FormValue("client_id")
-	if clientID != "123" {
-		jsonResponse(w, map[string]string{"error": "invalid client_id."}, http.StatusBadRequest)
-		return
-	}
-	jsonResponse(w, map[string]string{"privacy_policy_url": "http://localhost:8001/privacy_policy.html", "terms_of_service_url": "http://localhost:8001/terms_of_service.html"}, http.StatusOK)
 }
 
 func sessionCheckMiddleware(next http.HandlerFunc) http.HandlerFunc {
